57a2355394c46af6fc293c6223106970c2e3175d
isaacyi <iyi@researchinnovations.com>
2018-03-10 23:04:42 -0500

    first controlled component with push state

--- chasin-tails-menu/src/components/Entrance.js
+++ chasin-tails-menu/src/components/Entrance.js
@@ -2,15 +2,40 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class Entrance extends React.Component {
+  state = {
+    store: ''
+  };
+
+  enterStore = (e) => {
+    // prevent default behavior of input form submit action, which causes page refresh
+    e.preventDefault();
+    let storeId = this.state.store;
+    console.log(`Going to store: ${storeId}!`);
+    // push state: method from Router that changes route without a page reload, so no memory lost in any page
+    this.props.history.push(`/store/${storeId}`);
+  };
+
+  handleChange = (e) => {
+    // controlled component: state is the single source of truth
+    this.setState({ store: e.target.value });
+  };
+
   render () {
     return (
-      <form className="store-selector">
-        <h3>Please Enter A Store</h3>
-        <input required type="text" placeholder="Store Name" defaultValue={getFunName()}></input>
+      <form className="store-selector" onSubmit={this.enterStore}>
+        <h3>Please Enter Store Name</h3>
+        <input 
+        required 
+        type="text" 
+        placeholder={getFunName()}
+        value={this.state.store}
+        onChange={this.handleChange}
+        />
         <button type="submit">Goto Store -></button>
       </form>
     );
   }
+
 }
 
 export default Entrance;
