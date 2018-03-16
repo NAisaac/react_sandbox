1ff6c424250e8d3a9e8b280307c561243abc15c4
isaacyi <iyi@researchinnovations.com>
2018-03-15 22:30:23 -0400

    remove reduxstagram replaced redux-stagram

--- reduxstagram/client/reduxstagram.js
+++
@@ -1,9 +0,0 @@
-// any time something is imported from 'name' this means it's coming from node_modules
-import React from 'react';
-import { render } from 'react-dom';
-import css from './styles/style.styl';
-import Main from './components/Main';
-
-render(<Main/>, document.getElementById('root'));
-
-
